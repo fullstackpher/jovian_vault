3d4d913ccb9a2fa0a88f50769787a8a5f0ce7cc9
张乐 <le.zhang@highsoft.ltd>
2026-01-27 16:24:41 +0800

    vault backup: 2026-01-27 16:24:41

--- QuickAddMacros/calloutMaker.js
+++ QuickAddMacros/calloutMaker.js
@@ -4,7 +4,7 @@ module.exports = async (params) => {
     // 1. 选择类型
     const typeChoice = await quickadd.suggester(
         ["💡 重点", "❓ 疑问", "📌 摘要", "⚠️ 警告", "🔬 示例"],
-        ["tip", "question", "abstract", "warning", "example"]
+        ["tip", "question", "abstract", "danger", "example"]
     );
     if (!typeChoice) return;
 
